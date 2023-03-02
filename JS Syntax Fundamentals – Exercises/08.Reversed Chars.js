2955cdb3e0c1c228f81d7fc31cafc4fc5e8b678d
mims_3 <93831078+MariaRosenova@users.noreply.github.com>
2023-03-02 10:32:04 +0200

    Update 08.Reversed Chars.js

--- JS Syntax Fundamentals – Exercises/08.Reversed Chars.js
+++ JS Syntax Fundamentals – Exercises/08.Reversed Chars.js
@@ -1,18 +1,6 @@
 function reverse(firstP, secondP, thirdP) {
-    let array = [];
-    let arr=[firstP, secondP, thirdP];
-    arr.reverse();
-
-   for(let index = 0; index<arr.length; index++){
-        array += [index + ' '];
-   }
-
-   for(let index=0; index<array.length; index++){
-        console.log(array[index]);
-   }
+     let arr=[firstP,secondP,thirdP];
+     arr.reverse();
+     console.log(arr.join(' '));
 }
-//4ti slide v powerpoint :)
-// obicham te i bravo za usiliqta ti dnes!
-// gordeq se s teb
-
-reverse(3,2,1);
+reverse(1,2,3);
