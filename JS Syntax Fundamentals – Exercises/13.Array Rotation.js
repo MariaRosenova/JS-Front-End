051a2dc4cf0a007a18d184ed19796dda138eed65
mims_3 <93831078+MariaRosenova@users.noreply.github.com>
2023-03-03 18:39:17 +0200

    .

---
+++ JS Syntax Fundamentals – Exercises/13.Array Rotation.js
@@ -0,0 +1,12 @@
+function arrayRotations(numbers, rotations){
+    rotations %= numbers.length;
+
+    for (let index = 0; index < rotations; index++) {
+        let firstNum = numbers.shift();
+        numbers.push(firstNum);
+    }
+
+    console.log(numbers.join(' '));
+}
+
+arrayRotations([51, 47, 32, 61, 21], 2);
