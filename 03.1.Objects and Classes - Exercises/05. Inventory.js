83929c4859540f1b4d747591035998a425dfb2ad
mims_3 <93831078+MariaRosenova@users.noreply.github.com>
2023-03-21 09:50:01 +0200

    .

---
+++ 03.1.Objects and Classes - Exercises/05. Inventory.js
@@ -0,0 +1,22 @@
+function heroesList(input){
+    let heroes = [];
+    for (let line of input) {
+        let [hero, level, items] = line.split(' / ');
+        level = Number(level);
+        heroes.push({hero, level, items});
+    }
+    let sortedHeroes = heroes.sort((heroA, heroB) => heroA.level - heroB.level);
+    for(const {hero, level, items} of sortedHeroes){
+        console.log(`Hero: ${hero}`);
+        console.log(`level => ${level}`);
+        console.log(`items => ${items}`);
+    }
+}
+
+
+heroesList([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+    ]
+    );
