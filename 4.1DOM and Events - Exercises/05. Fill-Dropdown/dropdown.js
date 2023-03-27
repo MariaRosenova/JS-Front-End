62f8202b72814c7831ba2509b09bc91830450e66
mims_3 <93831078+MariaRosenova@users.noreply.github.com>
2023-03-27 20:32:26 +0300

    .

--- 4.1DOM and Events - Exercises/05. Fill-Dropdown/dropdown.js
+++ 4.1DOM and Events - Exercises/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,18 @@
 function addItem() {
-    console.log('TODO:...');
+    //document.querySelectorAll('input[type="button"]')
+    const newItemText = document.getElementById('newItemText');
+    const newItemValue = document.getElementById('newItemValue');
+    const menu = document.getElementById('menu');
+    
+    const textValue = newItemText.value;
+    const textIValue = newItemValue.value;
+    const option = document.createElement('option');
+    option.textContent = textValue;
+    option.value = textIValue;
+    menu.appendChild(option);
+
+    newItemText.value = '';
+    newItemValue.value = '';
+    
+   
 }
