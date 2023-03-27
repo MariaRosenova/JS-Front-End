cc32d423c74b249b5ebfbf4ec9296cba61ef0277
mims_3 <93831078+MariaRosenova@users.noreply.github.com>
2023-03-27 19:06:19 +0300

    .

--- 4.1DOM and Events - Exercises/04. Locked-Profile/app.js
+++ 4.1DOM and Events - Exercises/04. Locked-Profile/app.js
@@ -1,3 +1,27 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const btn = Array.from(document.getElementsByTagName('button'))
+    .forEach((button) => {
+        button.addEventListener('click', toggleInfo);
+    });
+
+    function toggleInfo(e){
+        const btn = e.currentTarget;
+        const currentProfile = btn.parentElement;
+        const children = Array.from(currentProfile.children);
+        const additionalInfo = children[9];
+        const UnlockRadioButton = children[4];
+        console.log(additionalInfo);
+
+        if (UnlockRadioButton.checked) {
+            if (btn.textContent === 'Show more') {
+                additionalInfo.style.display = "block";
+                btn.textContent = "Hide it"
+            }
+            else{
+                additionalInfo.style.display = "none";
+                btn.textContent = "Show more";
+            }
+    
+        }              
+    }
 }
