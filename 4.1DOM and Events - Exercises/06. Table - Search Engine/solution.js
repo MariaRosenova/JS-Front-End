3430b0e648616882a08cdebf4145b35516723014
mims_3 <93831078+MariaRosenova@users.noreply.github.com>
2023-03-29 21:21:58 +0300

    .

--- 4.1DOM and Events - Exercises/06. Table - Search Engine/solution.js
+++ 4.1DOM and Events - Exercises/06. Table - Search Engine/solution.js
@@ -1,8 +1,23 @@
 function solve() {
+   const inputText = document.getElementById('searchField');
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+
+      const searchedWord = inputText.value;
+      const tableRows = Array.from(document.querySelectorAll('tbody tr'));
+      
+      for (const row of tableRows) {
+         let trimmedTextContent = row.textContent.trim();
+
+         if (row.classList.contains('select')) {
+            row.classList.remove('select');
+         }
+         
+         if(trimmedTextContent.includes(searchedWord)){
+            row.classList.add('select');
+         }
+      }
 
    }
 }
