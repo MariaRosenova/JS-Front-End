31c69df618c38fe87e1576d9a1169d8dbc6f83ae
mims_3 <93831078+MariaRosenova@users.noreply.github.com>
2023-03-30 10:44:52 +0300

    .

--- 4.1DOM and Events - Exercises/07. Format the Text/solution.js
+++ 4.1DOM and Events - Exercises/07. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+  const input = document.getElementById('input');
+  const output = document.getElementById('output');
+
+  const splitter = input.value.split('.');
+  splitter.pop();
+  console.log(splitter);
+  while (splitter.length > 0) {
+    let paragraph = splitter.splice(0,3)
+    .map((p) => p.trimStart());
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = paragraph + '.';
+    output.appendChild(newParagraph);
+  
+  }
 }
